219e78c1476599a2b5eb9a6a36cdd55878725c25
Raj Kamaria <raj.kamaria@kellogg.northwestern.edu>
2021-05-04 23:49:17 -0500

    Done1

--- week5.js
+++ week5.js
@@ -11,47 +11,35 @@
 //        Weather for Chicago, Illinois".
 // - Homework: Complete the application by accepting a number of days; show the current weather 
 //             conditions and forecast based on the number of days entered by the user.
-
 window.addEventListener('DOMContentLoaded', async function() {
-  
-    // Select "get weather" button
+     // Select "get weather" button
     let weatherButton = document.querySelector(`.get-weather`)
-  
-    // Then do something when clicked:
+     // Then do something when clicked:
     weatherButton.addEventListener(`click`, async function(event){
-  
-      // - Kill the function
+        // - Kill the function
       event.preventDefault()
-      
-      // - Select location and days
+  
+     // - Select location and days
       let location = document.querySelector(`#location`)
       let days = document.querySelector(`#days`)
-      
-      // - Grab their values
+            // - Grab their values
       let locationDetes = location.value
       let dayDetes = days.value
-      
-      // - If value entered, then...
+            // - If value entered, then...
       if (locationDetes.length>0 && dayDetes.length>0) {
-        
-        // - Make URL for WeatherAPI.com API, based on the values
+                // - Make URL for WeatherAPI.com API, based on the values
         let url = `https://api.weatherapi.com/v1/forecast.json?key=663b1ce3b2e54644b16153852212704&q=${locationDetes}&days=${dayDetes}`
-        
-        // - Get the url, wait for response
+                // - Get the url, wait for response
         let response = await fetch(url)
-        
-        // - Ask for the json-formatted data, wait for it
+                // - Ask for the json-formatted data, wait for it
         let json = await response.json()
         console.log(json)
-        
-        // - Store  location, current weather, forecast
+            // - Store  location, current weather, forecast
         let location.data = json.location
         let weather.current = json.current
         let weather.forecast = json.forecast
-        
-        // Select 'current' 
+            // Select 'current' 
         let Heading = document.querySelector(`.current`)
-        
         // Edit elements within
         Heading.innerHTML = `
           <div class="text-center space-y-2">
@@ -63,14 +51,12 @@ window.addEventListener('DOMContentLoaded', async function() {
               <span class="conditions">${weather.current.condition.text}</span>
             </div>
             <div class="font-bold text-3xl">${dayDetes} Day Forecast</div>
-          </div>
-        `
+          </div>`
           // Loop through number of days entered by user
         for (let i=0;i<dayDetes;i++){
         let forecast.detes = document.querySelector(`.forecast`)
         let weather.loop = weather.forecast.forecastday[i]
-        
-        // Insert appropriate HTML for # of days user entereds
+                // Insert appropriate HTML for # of days user entereds
         forecastdetes.insertAdjacentHTML(`beforeend`,`<div class="text-center space-y-8">
         <div>
           <img src="https:${weather.loop.day.condition.icon}" class="mx-auto">
